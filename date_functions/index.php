6989480749ed00f95fbded12559edd940f9131a3
em0194192 <147880851+em0194192@users.noreply.github.com>
2024-09-11 14:52:54 +0000

    Add files via upload

---
+++ date_functions/index.php
@@ -0,0 +1,32 @@
+<?php
+    include "functions.php";
+    $days="";
+    if (isset($_GET['numDays']))
+    {
+        $days = calculateFutureDate($_GET['numDays']);
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    
+    if($days)
+    {
+        echo("<h1> $days </h1>");
+    } else {
+        todaysDate();
+    }
+    
+    //calculateFutureDate();  //can call because it was included
+    include "form.php";
+?>
+
+</body>
+</html>
